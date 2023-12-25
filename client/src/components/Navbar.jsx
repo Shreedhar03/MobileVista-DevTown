9fe2b481502e7a63c234f2f0b80a5af574709a48
Shreedhar <urawane03@gmail.com>
2023-12-25 12:47:04 +0530

    search functionality

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,16 +1,19 @@
 import React, { useContext, useEffect, useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import cartimg from '../assets/cart.svg'
 import logo from '../assets/samsung.svg'
 import menu from '../assets/menu.svg'
 import ExpandDiv from './ExpandDiv'
 import { AppContext } from '../App'
+import Divider from './Divider'
 
 const Navbar = () => {
     const [height, setHeight] = useState(window.scrollY)
     const [navBg, setNavBg] = useState(false)
     const [expandNav, setExpandNav] = useState(false)
-    const { cart } = useContext(AppContext)
+    const [searching, setSearching] = useState(false)
+    const [searchResults, setSearchResults] = useState([])
+    const { allProducts, cart } = useContext(AppContext)
     useEffect(() => {
         console.log(height)
         window.addEventListener('scroll', () => {
@@ -20,6 +23,17 @@ const Navbar = () => {
         height > 60 ? setNavBg(true) : setNavBg(false)
     }, [height])
     const navigate = useNavigate()
+    const handleSearchChange = (e) => {
+        const searchTerm = e.target.value.toLowerCase();
+        setSearching(true);
+
+        const results = allProducts.filter((product) =>
+            product.productName.toLowerCase().includes(searchTerm)
+        );
+        searchTerm.length > 1 ? setSearchResults(results) : setSearchResults([]);
+        results.length == 0 && setSearching(false)
+        searchTerm.length < 2 && setSearching(false)
+    };
     return (
         <nav className={`sticky top-0 ${navBg ? 'bg-gray-100' : 'bg-white'} z-20 `}>
 
@@ -28,7 +42,7 @@ const Navbar = () => {
                 <img src={logo} alt="logo" className='w-24' onClick={() => navigate('/')} />
 
                 <ul className='flex flex-wrap gap-4 sm:gap-14'>
-                    <ExpandDiv url={'/products'} title={'All Smartphones'}/>
+                    <ExpandDiv url={'/products'} title={'All Smartphones'} />
                     <ExpandDiv products={[
                         { title: 'Galaxy Z Flip5', image: 'https://images.samsung.com/is/image/samsung/assets/in/2307/pcd/PCD_B5_Whats-new_376x376_pc_default.png?$376_376_PNG$' },
                         { title: 'Galaxy S23 Ultra', image: 'https://images.samsung.com/in/smartphones/galaxy-s23-ultra/images/galaxy-s23-ultra-highlights-kv.jpg' },
@@ -47,12 +61,30 @@ const Navbar = () => {
                     ]} title='Flagship' />
                 </ul>
                 {/* search box */}
-                <div className='flex items-center gap-3'>
-                    <input type="text" placeholder='search products' className='focus:outline-none border border-slate-300 rounded-xl px-4 py-2 text-sm' />
+                <div className='flex items-center mt-6 sm:mt-0 gap-3 relative'>
+                    <input type="text" onBlur={() => setTimeout(() => {
+                        setSearching(false)
+                    }, 1000)} onChange={handleSearchChange} placeholder='search products' className='focus:outline-none border border-slate-300 rounded-xl px-4 py-2 text-sm' />
                     <button className='relative' onClick={() => navigate('/checkout')}>
                         <img src={cartimg} alt="cart" className='w-6' />
-                        <p className={`absolute -top-1 -right-1 bg-red-700 ${cart.length==0 && 'hidden'} w-4 h-4 rounded-full flex items-center justify-center text-xs text-white`}>{cart.length>0 && cart.length}</p>
+                        <p className={`absolute -top-1 -right-1 bg-red-700 ${cart.length == 0 && 'hidden'} w-4 h-4 rounded-full flex items-center justify-center text-xs text-white`}>{cart.length > 0 && cart.length}</p>
                     </button>
+                    {/* <section className={`${searching || 'hidden' } flex w-full flex-col gap-3 rounded-xl absolute top-20 left-0 p-3 bg-gray-100`}>
+                        <Link to={`/product/`} className='flex items-center gap-2'>
+                            <img className='w-12' src="https://images.samsung.com/is/image/samsung/p6pim/in/sm-a145fzkdins/gallery/in-galaxy-a14-sm-a145-sm-a145fzkdins-thumb-536656216?$172_172_PNG$" alt="phone" />
+                            <h1 className='font-bold'>Galaxy A12+</h1>
+                        </Link>
+                        <Divider />
+                    </section> */}
+                    <section className={`${searching || 'hidden'} flex w-full max-h-72 overflow-y-scroll flex-col gap-3 rounded-xl absolute top-20 left-0 p-3 bg-gray-100`}>
+                        {searchResults.map((result, index) => (
+                            <Link key={index} to={`/product/${result.productName}`} className='flex items-center gap-2'>
+                                <img className='w-12' src={result.images[0]} alt={result.productName} />
+                                <h1 className='font-bold'>{result.productName}</h1>
+                            </Link>
+                        ))}
+                        {searchResults.length > 0 && <Divider />}
+                    </section>
                 </div>
 
                 <button className='absolute w-5 block sm:hidden top-9 right-10' onClick={() => setExpandNav(!expandNav)}>
--- client/src/pages/AllProducts.jsx
+++ client/src/pages/AllProducts.jsx
@@ -1,66 +1,84 @@
-import React, { useContext, useEffect, useState } from 'react'
-import Navbar from '../components/Navbar'
-import MobileCard from '../components/MobileCard'
-import down from '../assets/down.svg'
-import up from '../assets/up.svg'
-import { AppContext } from '../App'
+import React, { useContext, useEffect, useState } from 'react';
+import Navbar from '../components/Navbar';
+import MobileCard from '../components/MobileCard';
+import down from '../assets/down.svg';
+import up from '../assets/up.svg';
+import { AppContext } from '../App';
 
 const AllProducts = () => {
-    const [showFilters, setShowFilters] = useState(0)
-    const { allProducts } = useContext(AppContext)
+  const [showFilters, setShowFilters] = useState(false);
+  const [ramFilter, setRamFilter] = useState('All');
+  const [storageFilter, setStorageFilter] = useState('All');
+  const [processorFilter, setProcessorFilter] = useState('All');
 
-    useEffect(()=>{
-        window.scrollTo(0,0)
-    },[])
-    return (
-        <>
-            <Navbar />
+  const { allProducts } = useContext(AppContext);
 
-            <section className='max-w-6xl mx-6 xl:mx-auto my-6'>
-                <h2 className='text-lg flex items-center gap-1' onClick={() => setShowFilters(!showFilters)}>Apply Filters <img src={!showFilters ? down : up} alt="down" /></h2>
-                <div className={`${showFilters ? 'flex' : 'hidden'} flex gap-4 sm:gap-8 flex-wrap mt-3 mb-6`}>
-                    <div className='flex flex-col gap-3'>
-                        <h1 className='underline'>RAM</h1>
-                        <div className='flex flex-wrap gap-2 items-center'>
-                            <button className='bg-slate-800 text-white px-2 py-1 rounded-lg'>All</button>
-                            <button className='bg-slate-200 px-2 py-1 rounded-lg'>8GB</button>
-                            <button className='bg-slate-200 px-2 py-1 rounded-lg'>16GB</button>
-                            <button className='bg-slate-200 px-2 py-1 rounded-lg'>32GB</button>
-                        </div>
-                    </div>
-                    <div className='h-20 w-[2px] bg-slate-200 hidden xl:block'></div>
-                    <div className='flex flex-col gap-3'>
-                        <h1 className='underline'>Storage</h1>
-                        <div className='flex flex-wrap gap-2 items-center'>
-                            <button className='bg-slate-800 text-white px-2 py-1 rounded-lg'>All</button>
-                            <button className='bg-slate-200 px-2 py-1 rounded-lg'>64GB</button>
-                            <button className='bg-slate-200 px-2 py-1 rounded-lg'>128GB</button>
-                            <button className='bg-slate-200 px-2 py-1 rounded-lg'>256GB</button>
-                        </div>
-                    </div>
-                    <div className='h-20 w-[2px] bg-slate-200 hidden xl:block'></div>
-                    <div className='flex flex-col gap-3'>
-                        <h1 className='underline'>Processor</h1>
-                        <div className='flex flex-wrap gap-2 items-center'>
-                            <button className='bg-slate-800 text-white px-2 py-1 rounded-lg'>All</button>
-                            <button className='bg-slate-200 px-2 py-1 rounded-lg'>Dimensity</button>
-                            <button className='bg-slate-200 px-2 py-1 rounded-lg'>MediaTek</button>
-                            <button className='bg-slate-200 px-2 py-1 rounded-lg'>Snapdragon</button>
-                            <button className='bg-slate-200 px-2 py-1 rounded-lg'>Qualcomm</button>
-                        </div>
-                    </div>
-                </div>
-                <h1 className='text-lg mt-4'>Discover All Smartphones</h1>
-                <div className='flex flex-wrap items-center justify-center sm:justify-start gap-6 mt-6'>
-                    {
-                        allProducts.map((p,key) => {
-                            return <MobileCard id={p._id} key={key} brand={p.brand} productName={p.productName} images={p.images} discountPercentage={p.discountPercentage} price={p.price} rating={p.rating}/>
-                        })
-                    }
-                </div>
-            </section>
-        </>
-    )
-}
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
 
-export default AllProducts
+  const handleFilterChange = (filterType, value) => {
+    switch (filterType) {
+      case 'RAM':
+        setRamFilter(value);
+        break;
+      case 'Storage':
+        setStorageFilter(value);
+        break;
+      case 'Processor':
+        setProcessorFilter(value);
+        break;
+      default:
+        break;
+    }
+  };
+
+  const filteredProducts = allProducts.filter((product) => {
+    const isRamMatch = ramFilter === 'All' || product.RAM === ramFilter;
+    const isStorageMatch = storageFilter === 'All' || product.storage === storageFilter;
+    const isProcessorMatch = processorFilter === 'All' || product.processor === processorFilter;
+
+    return isRamMatch && isStorageMatch && isProcessorMatch;
+  });
+
+  return (
+    <>
+      <Navbar />
+
+      <section className='max-w-6xl mx-6 xl:mx-auto my-6'>
+        <h2 className='text-lg flex items-center gap-1' onClick={() => setShowFilters(!showFilters)}>
+          Apply Filters <img src={!showFilters ? down : up} alt="down" />
+        </h2>
+        <div className={`${showFilters ? 'flex' : 'hidden'} flex gap-4 sm:gap-8 flex-wrap mt-3 mb-6`}>
+          <FilterGroup title="RAM" filters={['All', '8GB', '16GB', '32GB']} selectedFilter={ramFilter} onFilterChange={(value) => handleFilterChange('RAM', value)} />
+          <FilterGroup title="Storage" filters={['All', '64GB', '128GB', '256GB']} selectedFilter={storageFilter} onFilterChange={(value) => handleFilterChange('Storage', value)} />
+          <FilterGroup title="Processor" filters={['All', 'Dimensity', 'MediaTek', 'Snapdragon', 'Qualcomm']} selectedFilter={processorFilter} onFilterChange={(value) => handleFilterChange('Processor', value)} />
+        </div>
+        <h1 className='text-lg mt-4'>Discover All Smartphones</h1>
+        <div className='flex flex-wrap items-center justify-center sm:justify-start gap-6 mt-6'>
+          {filteredProducts?.map((p, key) => {
+            return <MobileCard id={p._id} key={key} brand={p.brand} productName={p.productName} images={p.images} discountPercentage={p.discountPercentage} price={p.price} rating={p.rating} />;
+          })}
+        </div>
+      </section>
+    </>
+  );
+};
+
+const FilterGroup = ({ title, filters, selectedFilter, onFilterChange }) => {
+  return (
+    <div className='flex flex-col gap-3'>
+      <h1 className='underline'>{title}</h1>
+      <div className='flex flex-wrap gap-2 items-center'>
+        {filters.map((filter, index) => (
+          <label key={index} className={`px-2 py-1 rounded-lg ${selectedFilter === filter ? 'bg-slate-800 text-white' : 'bg-slate-200'}`}>
+            <input type="radio" className='hidden' name={title} value={filter} checked={selectedFilter === filter} onChange={() => onFilterChange(filter)} />
+            {filter}
+          </label>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default AllProducts;
